d981a599e08e35cf8392d4caaaafe86d7829387f
dbsdud00 <dbsdudsw@icloud.com>
2023-11-09 22:27:33 +0900

    11

--- src/index.js
+++ src/index.js
@@ -4,10 +4,30 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type == "ADD") {
+    console.log("add call");
+    return count + 1;
+  } else if (action.type == "MINUS") {
+    return count - 1;
+  }
+  return count;
 };
 
 const countStore = createStore(countModifier);
 
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
 console.log(countStore.getState());
